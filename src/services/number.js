1d33db37e6071c7d063a390b82aaed14ed43eb54
eugene choi <eugene.maestro.choi@gmail.com>
2023-02-10 16:01:35 +0900

    add number format service

---
+++ src/services/number.js
@@ -0,0 +1,40 @@
+import {
+  format, unformat,
+} from 'number-currency-format';
+
+export const roundTo = (number, precision, type = 'round') => {
+  const power = 10 ** precision;
+
+  if (type === 'round') return Math.round(number * power) / power;
+  if (type === 'down') return Math.floor(number * power) / power;
+  if (type === 'up') return Math.ceil(number * power) / power;
+
+  return Math.round(number * power) / power;
+};
+
+export const formatNumber = (v, opts = {}) => {
+  return format(v, {
+    spacing: false,
+    decimalsDigits: 0,
+    showDecimals: 'NEVER',
+    ...opts,
+  });
+};
+export const formatCurrency = (v, opts = {}) => {
+  return format(v, {
+    currency: '원',
+    currencyPosition: 'RIGHT',
+    spacing: false,
+    decimalsDigits: 0,
+    showDecimals: 'NEVER',
+    ...opts,
+  });
+};
+
+export const roundCurrency = (v = 0, others = {}, roundType = 'up', precision = 2) => {
+  const rounded = roundTo(v, precision, roundType);
+  return unformat(format(rounded, {
+    decimalsDigits: 0,
+    ...others,
+  }));
+};
