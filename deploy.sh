106e56f9ddaa74359e15f4806225c73449e4ee74
eugene choi <eugene.maestro.choi@gmail.com>
2023-02-16 18:16:26 +0900

    add deploy.sh for creo test server

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+
+#!/bin/sh
+# Deploy Script
+# Quick deployment to ssh server
+
+now="$(date +'%d/%m/%Y/%r')"
+echo $now
+
+# build
+npm run build
+
+# set up new git remote, add, commit to today's datetime
+cd build
+git init .
+git add --all
+git commit -m "$now"
+git remote add deploy root@test.creokorea.com:/data/project/erp.git
+
+# push to post receive remote
+git push deploy master --force
+
