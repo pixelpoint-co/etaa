a083732051d8973de324672107b840e19c8e0563
eugene choi <eugene.maestro.choi@gmail.com>
2023-02-27 21:42:59 +0900

    add LabelValue component

---
+++ src/components/molecules/LabelValue/index.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import { palette } from 'styled-theme';
+import {
+  ifProp, switchProp, prop,
+} from 'styled-tools';
+
+import Flex from '../../atoms/Flex';
+import Text from '../../atoms/P';
+
+const Wrapper = styled(Flex)`
+  flexDirection: row;
+  flex: 1;
+  padding: 10px;
+`;
+
+const Label = styled(Text)`
+  flex: 1;
+  font-size: 14px;
+  align-self: flex-start;
+  color: ${palette('grayscale', 0)};
+  font-weight: ${ifProp('bold', 800, 400)};
+`;
+
+const Value = styled(Text)`
+  font-size: 14px;
+  align-self: flex-end;
+  color: ${palette('black', 0)};
+  font-weight: ${ifProp('bold', 800, 400)};
+`;
+const LabelValue = ({
+  label,
+  value,
+  labelStyle,
+  valueStyle,
+  bold,
+  ...others
+}) => {
+  return (
+    <Wrapper
+      {...others}
+    >
+      <Label
+        style={labelStyle}
+        bold={bold}
+      >
+        {label}
+      </Label>
+      <Value
+        style={valueStyle}
+        bold={bold}
+      >
+        {value}
+      </Value>
+    </Wrapper>
+  );
+};
+
+export default LabelValue;
