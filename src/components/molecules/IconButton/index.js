c7e1fbe2b2f8687cf0992de082ddbfbd3f92f1a1
eugene choi <eugene.maestro.choi@gmail.com>
2023-05-14 17:16:31 +0900

    add iconbutton

---
+++ src/components/molecules/IconButton/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, {
+  css,
+} from 'styled-components';
+import {
+  switchProp, ifProp, prop,
+} from 'styled-tools';
+
+import Icon from '../../atoms/Icon';
+import Button from '../../atoms/Button';
+import Flex from '../../atoms/Flex';
+
+const StyledButton = styled(Button)`
+  max-width: 100%;
+`;
+
+const Wrapper = styled(Flex)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const IconButton = (props) => {
+  const {
+    icon,
+    iconSize = 16,
+    style,
+    ...others
+  } = props;
+  return (
+    <StyledButton
+      {...others}
+    >
+      <Wrapper>
+        <Icon
+          icon={icon}
+          size={iconSize}
+        />
+      </Wrapper>
+    </StyledButton>
+  );
+};
+
+IconButton.propTypes = { icon: PropTypes.string.isRequired };
+
+IconButton.defaultProps = {};
+
+export default IconButton;
