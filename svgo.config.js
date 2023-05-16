509e9c98dd2a9c30202357735d00ece1401ed149
eugene choi <eugene.maestro.choi@gmail.com>
2023-05-16 11:58:30 +0900

    add config for svgo to ensure unique clip path

---
+++ svgo.config.js
@@ -0,0 +1,8 @@
+// svgo.config.js
+module.exports = {
+  js2svg: {
+    indent: 2, // string with spaces or number of spaces. 4 by default
+    pretty: true, // boolean, false by default
+  },
+  plugins: ['prefixIds'],
+};
