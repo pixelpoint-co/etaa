0f87881bc6cfc28290bbeeff61862e763e9ef429
eugene choi <eugene.maestro.choi@gmail.com>
2023-07-13 15:46:14 +0900

    remove auto rotate

--- src/hooks/usePotController.js
+++ src/hooks/usePotController.js
@@ -384,19 +384,18 @@ const usePotController = (cookerId, opts = {}) => {
       setCookStartUUID(uuidv4());
       setLastActionType('recipe');
       setLastActionId(21);
-      setTimeout(
-        () => {
-          kitchenPotRotationSwitch({ variables: { cookerId } });
-        },
-        2000,
-      );
+      // setTimeout(
+      //   () => {
+      //     kitchenPotRotationSwitch({ variables: { cookerId } });
+      //   },
+      //   2000,
+      // );
     },
     [
-      cookerId,
-      kitchenPotRotationSwitch,
+      // cookerId,
+      // kitchenPotRotationSwitch,
 
-      selectedOrderKitchenId,
-    ],
+      selectedOrderKitchenId],
   );
 
   const potRotationSwitch = useCallback(
