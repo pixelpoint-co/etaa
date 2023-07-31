ab63bc6e3b91002dd1966ff949d548b61c9dc920
eugene choi <eugene.maestro.choi@gmail.com>
2023-07-31 16:26:36 +0900

    prod url

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4100';
+const URL = process.env.NODE_ENV === 'production' ? 'http://192.168.0.100:4100' : 'http://localhost:4100';
 
 export const socket = io(URL);
