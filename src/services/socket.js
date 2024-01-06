e148e0fe270b1939c85ca8b73d72e6aa4ab17a50
eugene choi <eugene.maestro.choi@gmail.com>
2024-01-06 16:47:12 +0900

    socket transport is limited to websocket for non-sticky session handling https://socket.io/docs/v4/using-multiple-nodes/\#why-is-sticky-session-required

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,5 +1,4 @@
 import { io } from 'socket.io-client';
-
 // "undefined" means the URL will be computed from the `window.location` object
 const currentUrl = new URL(window.location);
 
@@ -11,13 +10,11 @@ const isRemote = (
 ) || process.env.REACT_APP_IS_REMOTE;
 const url = isRemote ? process.env.REACT_APP_CHEF_URL_REMOTE : process.env.REACT_APP_CHEF_URL;
 
-// const url = process.env.REACT_APP_CHEF_URL;
-console.log(process.env);
 export const socket = io(
   url,
   {
-    withCredentials: process.env.REACT_APP_ENV === 'production',
-    // query
+    withCredentials: true,
+    transports: ['websocket'],
   },
 );
 document?.addEventListener(
